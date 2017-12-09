49ef241d704fe99bad779b005981db18660c92dd
Michael Bramblett <michaelbramblett@michaels-macbook-pro.local>
2017-12-08 19:49:04 -0700

    Initial Commit

---
+++ Stackskills Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stackskills Project
+//
+//  Created by Michael Bramblett on 12/8/17.
+//  Copyright © 2017 Michael Bramblett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
